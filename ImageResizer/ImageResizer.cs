b270bb156fa3c1b62a71c645323e941761bc6562
Hugo <ghugobrunelle@gmail.com>
2022-11-14 21:15:53 -0500

    Add project files.

---
+++ ImageResizer/ImageResizer.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ImageResizer
+{
+    internal class ImageResizer
+    {
+        public static void resize(string target, string output, int width, int height)
+        {
+            try
+            {
+                FileStream file = File.OpenRead(target);
+                Image image = Image.FromStream(file);
+                byte[] imagesBytes = toByteArray(image, width, height);
+                writeToDisk(imagesBytes, output);
+            } 
+            catch(Exception e)
+            {
+                Console.WriteLine("Could'nt process the images");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static byte[] toByteArray(Image image, int width, int height)
+        {
+            Bitmap resized = new Bitmap(image, new Size(width, height));
+            using var imageStream = new MemoryStream();
+            resized.Save(imageStream, ImageFormat.Jpeg);
+            return imageStream.ToArray();
+        }
+
+        private static void writeToDisk(byte[] data, string output)
+        {
+            using (var stream = new FileStream(output, FileMode.Create, FileAccess.Write, FileShare.Write, 4096))
+            {
+                stream.Write(data, 0, data.Length);
+            }
+        }
+    }
+}
---
+++ ImageResizer/Program.cs
@@ -0,0 +1,61 @@
+﻿Console.WriteLine("====================================================");
+Console.WriteLine("         Image Resizer v1.0 by Hugo Brunelle ");
+Console.WriteLine("====================================================");
+
+
+Console.WriteLine("");
+Console.WriteLine("");
+
+Console.Write("\t\tTarget folder: ");
+string target = Console.ReadLine();
+if (!Directory.Exists($"{Directory.GetCurrentDirectory()}\\{target}"))
+{
+    Console.WriteLine("Folder does not exists");
+    return -1;
+}
+
+Console.Write("\t\tWidth: ");
+string widthString = Console.ReadLine();
+int width = 0;
+if (!Int32.TryParse(args[1], out width))
+{
+    Console.WriteLine("Width wrong format");
+    return -1;
+}
+
+Console.Write("\t\tHeight: ");
+string heightString = Console.ReadLine();
+int height = 0;
+if (!Int32.TryParse(args[2], out height))
+{
+    Console.Write("Height wrong format");
+    return -1;
+}
+
+string[] images = Directory.GetFiles(target);
+if (images.Length == 0)
+{
+    Console.WriteLine("No images found inside the folder");
+    return -1;
+}
+
+string output = $"{Directory.GetCurrentDirectory()}\\output\\thumbnails";
+if (!Directory.Exists(output))
+{
+    Directory.CreateDirectory(output);
+}
+
+Console.WriteLine("");
+Console.WriteLine("\tAlright young children, Processing images...");
+
+foreach (string img in images)
+{
+    ImageResizer.ImageResizer.resize(img, $"{output}\\{Path.GetFileName(img)}", width, height);
+}
+
+Console.WriteLine("\tResizing Complete !");
+Console.WriteLine("");
+Console.WriteLine("\tPress any key to continue...");
+Console.ReadKey();
+
+return 1;
